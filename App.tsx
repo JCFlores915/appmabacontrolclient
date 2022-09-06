54b16b9f5980e578ce13f35d73c8225f97e0f75d
Juan Carlos Flores Gutierrez <juanflores915@yahoo.es>
2022-09-06 13:17:48 -0600

    funcionalidad del splash screen para android

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
-import React, {ReactElement} from 'react';
+import React, {ReactElement, useEffect} from 'react';
 
 import { Navigation } from './src/navigation';
 
+import SplashScreen from 'react-native-splash-screen'
+
+
 const App: React.FC = (): ReactElement => {
   
+  useEffect(()=>{
+    SplashScreen.hide();
+  },[]);
+
   return (
     <Navigation />
   );
 };
-
-
 
 export default App;
